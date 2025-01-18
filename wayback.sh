853726e9d15def5fe7f91b6801bf2bbd9b40eb85
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-01-18 23:19:24 +0530

    Add files via upload

---
+++ wayback.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Function to display the menu
+show_menu() {
+    clear
+    echo -e "\033[0;37m"
+echo "     ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+echo "     ░  ░░░░  ░░░      ░░░  ░░░░  ░░       ░░░░      ░░░░      ░░░  ░░░░  ░"
+echo "     ▒  ▒  ▒  ▒▒  ▒▒▒▒  ▒▒▒  ▒▒  ▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒  ▒▒"
+echo "     ▓        ▓▓  ▓▓▓▓  ▓▓▓▓    ▓▓▓▓       ▓▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓▓▓▓▓     ▓▓▓▓"
+echo "     █   ██   ██        █████  █████  ████  ██        ██  ████  ██  ███  ██"
+echo "     █  ████  ██  ████  █████  █████       ███  ████  ███      ███  ████  █"
+echo "     ██████████████████████████████████████████████████████████████████████"
+    echo "                           MADE BY:INDIAN CYBER ARMY"
+    echo "          YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5"
+    echo -e "\033[0;36m __________________________INFORMATION GATHERING TOOL__________________________ "
+    echo ""
+    echo " [1] WAYBACKURL"
+    echo " [2] WAYBACKROBOT"
+    echo " [3] WAYBACKROBOTLATEST"
+    echo -e " [0]\033[0;31m EXIT"
+    echo -e "\033[0;36m"
+    echo -n " Please select an option: "
+}
+
+# Function to handle the user's selection
+handle_choice() {
+    case $1 in
+        1) bash src/waybackurl.sh;;
+        2) bash src/waybackrobot.sh;;
+        3) bash src/waybackrobotlatest.sh;;
+        0) echo "Exiting..."; exit 0;;
+        *) echo "Invalid option";;
+    esac
+}
+
+# Main loop to show the menu and handle choices
+while true; do
+    show_menu
+    read choice
+    handle_choice $choice
+    echo -e "\nPress any key to return to the menu..."
+    read -n 1
+done
+
